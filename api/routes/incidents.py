01d4f869d19618d05038fb8fdd296db0f7cc755f
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-05-06 23:11:58 +0700

    Update incidents.py

--- api/routes/incidents.py
+++ api/routes/incidents.py
@@ -1,8 +1,22 @@
+from fastapi import APIRouter, Depends, HTTPException
+from firebase_admin import db
+import time
+import json
+
+from api.models import EncryptedRequest
+from api.crypto import decrypt_message
+from api.deps import verify_token
+
+router = APIRouter(prefix="/api/incidents", tags=["incidents"])
+
 @router.post("/")
 async def create_incident(enc: EncryptedRequest, user=Depends(verify_token)):
     uid = user["uid"]
 
-    data = json.loads(decrypt_message(enc.data))
+    try:
+        data = json.loads(decrypt_message(enc.data))
+    except:
+        raise HTTPException(400, "Invalid encrypted data")
 
     db.reference("incidents").push({
         "lat": data["lat"],
