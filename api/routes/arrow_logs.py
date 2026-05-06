de54a92e546bf74b54647291bf79f369099e76d5
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-05-07 06:21:58 +0700

    Update arrow_logs.py

--- api/routes/arrow_logs.py
+++ api/routes/arrow_logs.py
@@ -10,17 +10,14 @@ from api.deps import verify_token
 router = APIRouter(prefix="/api/arrow-logs", tags=["arrow-logs"])
 
 @router.post("/")
-async def log_arrow(enc: EncryptedRequest, user=Depends(verify_token)):
-    try:
-        data = json.loads(decrypt_message(enc.data))
-    except:
-        raise HTTPException(400, "Invalid encrypted data")
-
+async def create_arrow_log(enc: EncryptedRequest, user=Depends(verify_token)):
+    data = decrypt(enc.data)
     db.reference("arrow_logs").push({
-        "from_uid": data["from_uid"],
-        "to_uid": data["to_uid"],
-        "coordinates": data["coordinates"],
+        "action": data["action"],
+        "drawingId": data.get("drawingId"),
+        "drawingAuthor": data.get("drawingAuthor"),
+        "performedBy": data.get("performedBy", user["uid"]),
+        "performedByName": data.get("performedByName"),
         "timestamp": data.get("timestamp", int(time.time() * 1000))
     })
-
     return {"success": True}
