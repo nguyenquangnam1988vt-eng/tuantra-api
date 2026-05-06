3212de5068d54627f558d3fb90888547beca990c
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-05-07 06:26:53 +0700

    Update models.py

--- api/models.py
+++ api/models.py
@@ -58,3 +58,15 @@ class DrawingPayload(BaseModel):
     timestamp: int
     type: str   
 
+class SpecialPointPayload(BaseModel):
+    lat: float
+    lng: float
+    type: str
+    typeLabel: str
+    icon: str
+    color: str
+    note: str
+    createdBy: str
+    createdById: str
+    timestamp: int
+
