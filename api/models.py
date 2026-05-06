781407ac4b11f8d22e5abe856956aced2f514b6f
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-05-07 01:03:57 +0700

    Update models.py

--- api/models.py
+++ api/models.py
@@ -49,11 +49,3 @@ class TrackPayload(BaseModel):
     lng: float
     timestamp: int
 
-class DrawingPayload(BaseModel):
-    points: List[Dict[str, float]]   
-    color: str
-    weight: int
-    author: str
-    authorId: str
-    timestamp: int
-    type: str   
