28b55af48e7c0b3222bad9520c6d52325b68bdd7
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-05-07 06:21:02 +0700

    Create drawings.py

---
+++ api/routes/drawings.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends, HTTPException
+from firebase_admin import db
+import time
+from api.models import EncryptedRequest
+from api.crypto import decrypt
+from api.deps import verify_token
+
+router = APIRouter(prefix="/api/drawings", tags=["drawings"])
+
+@router.post("/")
+async def create_drawing(enc: EncryptedRequest, user=Depends(verify_token)):
+    uid = user["uid"]
+    try:
+        data = decrypt(enc.data)
+    except Exception:
+        raise HTTPException(400, "Invalid encrypted data")
+
+    db.reference("drawings").push({
+        "points": data["points"],
+        "color": data["color"],
+        "weight": data["weight"],
+        "author": data["author"],
+        "authorId": uid,
+        "timestamp": data.get("timestamp", int(time.time() * 1000)),
+        "type": data["type"]
+    })
+    return {"success": True}
