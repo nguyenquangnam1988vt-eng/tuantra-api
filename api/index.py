9c89e46b724f1ec872d4ea7d9edb66074957c98e
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-05-07 06:27:15 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,6 +10,7 @@ from api.routes.arrow_logs import router as arrow_logs
 from api.routes.tracks import router as tracks
 from api.firebase import init_firebase
 from api.routes.drawings import router as drawings 
+from api.routes.special_points import router as special_points
 
 app = FastAPI()
 
@@ -25,6 +26,7 @@ app.include_router(users)
 app.include_router(tactical_plans)
 app.include_router(arrow_logs)
 app.include_router(drawings) 
+app.include_router(special_points)
 
 @app.get("/api/health")
 async def health():
