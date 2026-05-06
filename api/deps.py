f3c83f1785af0f064c711a32d9e9ec8eb709a48b
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-05-06 21:52:39 +0700

    Update deps.py

--- api/deps.py
+++ api/deps.py
@@ -1,7 +1,10 @@
 from fastapi import Header, HTTPException
 from firebase_admin import auth, db
+from api.firebase import init_firebase
 
 async def verify_token(authorization: str = Header(None)):
+    init_firebase()
+
     if not authorization:
         raise HTTPException(401, "Missing token")
 
@@ -12,6 +15,8 @@ async def verify_token(authorization: str = Header(None)):
     except:
         raise HTTPException(401, "Invalid token")
 
+
 def get_user_role(uid: str):
+    init_firebase()
     role = db.reference(f"users/{uid}/role").get()
     return role or "officer"
