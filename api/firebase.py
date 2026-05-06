c6a48b65f80ca58b6bcd30c968903fbf32f70ee4
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-05-06 23:09:40 +0700

    Update firebase.py

--- api/firebase.py
+++ api/firebase.py
@@ -8,11 +8,15 @@ def init_firebase():
         return
 
     cred_json = os.getenv("FIREBASE_CRED_JSON")
+    db_url = os.getenv("FIREBASE_DATABASE_URL")
+
     if not cred_json:
         raise Exception("Missing FIREBASE_CRED_JSON")
+    if not db_url:
+        raise Exception("Missing FIREBASE_DATABASE_URL")
 
     cred = credentials.Certificate(json.loads(cred_json))
 
     firebase_admin.initialize_app(cred, {
-        "databaseURL": os.getenv("FIREBASE_DATABASE_URL")
+        "databaseURL": db_url
     })
