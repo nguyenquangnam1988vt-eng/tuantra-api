536cda7575b5a621cfa3be576da88f219c5c7059
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-05-06 22:19:22 +0700

    Update crypto.py

--- api/crypto.py
+++ api/crypto.py
@@ -1,44 +1,22 @@
 import os
-import json
 import base64
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
-KEY = os.getenv("GROUP_KEY", "0123456789abcdef0123456789abcdef").encode()
+GROUP_KEY = os.getenv("GROUP_KEY").encode()
 
-def encrypt(data: dict) -> str:
-    iv = os.urandom(12)
-
-    cipher = Cipher(
-        algorithms.AES(KEY),
-        modes.GCM(iv),
-        backend=default_backend()
-    )
-
-    enc = cipher.encryptor()
-
-    plaintext = json.dumps(data).encode()
-
-    ciphertext = enc.update(plaintext) + enc.finalize()
-
-    return base64.b64encode(iv + enc.tag + ciphertext).decode()
-
-
-def decrypt(token: str) -> dict:
-    raw = base64.b64decode(token)
+def decrypt_message(data: str):
+    raw = base64.b64decode(data)
 
     iv = raw[:12]
     tag = raw[12:28]
     ciphertext = raw[28:]
 
     cipher = Cipher(
-        algorithms.AES(KEY),
+        algorithms.AES(GROUP_KEY),
         modes.GCM(iv, tag),
         backend=default_backend()
     )
 
-    dec = cipher.decryptor()
-
-    plaintext = dec.update(ciphertext) + dec.finalize()
-
-    return json.loads(plaintext.decode())
+    decryptor = cipher.decryptor()
+    return decryptor.update(ciphertext) + decryptor.finalize()
