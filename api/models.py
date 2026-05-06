6b2e725dd11736bfb229ef7b478ee28e980ff09f
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-05-06 22:11:26 +0700

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,15 +1,18 @@
 from pydantic import BaseModel
 
+class EncryptedRequest(BaseModel):
+    data: str
+
 class AlertModel(BaseModel):
     lat: float
     lng: float
     name: str
 
-class MarkerModel(BaseModel):
-    lat: float
-    lng: float
-
 class IncidentModel(BaseModel):
     lat: float
     lng: float
     image_url: str
+
+class MarkerModel(BaseModel):
+    lat: float
+    lng: float
