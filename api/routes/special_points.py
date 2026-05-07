514bc2c5e603b8604db70f75dbc68a97b9e13316
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-05-07 08:22:29 +0700

    Update special_points.py

--- api/routes/special_points.py
+++ api/routes/special_points.py
@@ -3,7 +3,7 @@ from firebase_admin import db
 import time
 import json
 from api.models import EncryptedRequest
-from api.crypto import decrypt
+from api.crypto import decrypt_message
 from api.deps import verify_token, get_user_role
 
 router = APIRouter(prefix="/api/special-points", tags=["special-points"])
