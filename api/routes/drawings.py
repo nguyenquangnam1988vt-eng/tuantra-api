f2b798d0320a49000488fef71e42696dc7f6f9a5
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-05-07 08:24:56 +0700

    Update drawings.py

--- api/routes/drawings.py
+++ api/routes/drawings.py
@@ -13,7 +13,7 @@ router = APIRouter(prefix="/api/drawings", tags=["drawings"])
 async def create_drawing(enc: EncryptedRequest, user=Depends(verify_token)):
     uid = user["uid"]
     try:
-        data = decrypt(enc.data)
+        decrypt_message(enc.data)
     except Exception:
         raise HTTPException(400, "Invalid encrypted data")
 
